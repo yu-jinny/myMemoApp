8a5e2f935daf5749623df546baae06dec3f55da0
yu-jinny <151537809+yu-jinny@users.noreply.github.com>
2024-01-09 23:02:23 +0900

    Initial Commit

---
+++ myMemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myMemoApp
+//
+//  Created by t2023-m0028 on 1/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
