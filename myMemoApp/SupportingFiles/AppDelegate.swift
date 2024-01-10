587cb1d7cc0ec607dd57107ba79ee734f01d1f2b
yu-jinny <151537809+yu-jinny@users.noreply.github.com>
2024-01-10 23:46:15 +0900

    Lv2.

--- myMemoApp/SupportingFiles/AppDelegate.swift
+++ myMemoApp/SupportingFiles/AppDelegate.swift
@@ -1,36 +1,28 @@
-//
 //  AppDelegate.swift
-//  myMemoApp
-//
-//  Created by t2023-m0028 on 1/9/24.
-//
 
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // 애플리케이션이 시작될 때의 사용자 정의 지점.
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
+        // 새로운 씬 세션을 만들 때 호출됨.
+        // 새로운 씬을 만들기 위한 설정(configuration)을 선택하는 메서드입니다.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        // 사용자가 씬 세션을 폐기할 때 호출됨.
+        // 애플리케이션이 실행되지 않은 동안에 폐기된 세션이 있다면,
+        // 이 메서드는 application:didFinishLaunchingWithOptions: 이후 짧은 시간 후에 호출됩니다.
+        // 폐기된 씬에 특정한 리소스를 해제하기 위해 이 메서드를 사용합니다.
     }
-
-
 }
 
--- myMemoApp/Views/TableViewCell.swift
+++ myMemoApp/Views/TableViewCell.swift
@@ -1,23 +1,40 @@
-//
-//  TableViewCell.swift
-//  myMemoApp
-//
-//  Created by t2023-m0028 on 1/10/24.
-//
+// TableViewCell.swift
 
 import UIKit
 
+protocol TableViewCellDelegate: AnyObject {
+    func switchChanged(for cell: TableViewCell)
+}
+
 class TableViewCell: UITableViewCell {
+    static let identifier = "cell"
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    weak var delegate: TableViewCellDelegate?
+    
+    func configure(with todoItem: TodoItem) {
+        textLabel?.text = todoItem.text
+
+        let switchView = UISwitch()
+        switchView.isOn = todoItem.isCompleted
+        switchView.addTarget(self, action: #selector(switchChanged(_:)), for: .valueChanged)
+
+        switchView.tag = tag
+        accessoryView = switchView
+
+        updateTextStrikeThrough(isCompleted: todoItem.isCompleted)
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    @objc func switchChanged(_ sender: UISwitch) {
+        delegate?.switchChanged(for: self)
     }
 
+    func updateTextStrikeThrough(isCompleted: Bool) {
+        if isCompleted {
+            let attributeString = NSAttributedString(string: textLabel?.text ?? "", attributes: [NSAttributedString.Key.strikethroughStyle: NSUnderlineStyle.single.rawValue])
+            textLabel?.attributedText = attributeString
+        } else {
+            let attributeString = NSAttributedString(string: textLabel?.text ?? "")
+            textLabel?.attributedText = attributeString
+        }
+    }
 }
