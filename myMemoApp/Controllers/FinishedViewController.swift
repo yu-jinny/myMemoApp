90443310f1b05a6704779f7473ddf1ad46283954
yu-jinny <151537809+yu-jinny@users.noreply.github.com>
2024-01-11 02:56:33 +0900

    LV2.

---
+++ myMemoApp/Controllers/FinishedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FinshedViewController.swift
+//  myMemoApp
+//
+//  Created by t2023-m0028 on 1/10/24.
+//
+
+import UIKit
+
+class FinshedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
